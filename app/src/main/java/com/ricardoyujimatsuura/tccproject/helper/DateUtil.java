609f19bd2c008f676a14b92df1273c6ba916ec19
Ricardo Matsuura <ricardoyujimatsuura@gmail.com>
2022-09-23 21:45:34 -0300

    criando a branch da tela principal do app

--- app/src/main/java/com/ricardoyujimatsuura/tccproject/helper/DateUtil.java
+++ app/src/main/java/com/ricardoyujimatsuura/tccproject/helper/DateUtil.java
@@ -1,4 +1,12 @@
 package com.ricardoyujimatsuura.tccproject.helper;
 
+import java.text.SimpleDateFormat;
+
 public class DateUtil {
+        public static String currentDate() {
+            long date = System.currentTimeMillis();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d/M/yyyy");
+            return simpleDateFormat.format(date);
+
+    }
 }
